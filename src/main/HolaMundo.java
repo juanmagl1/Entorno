6baa5feee014654482905d72586ed3294c0fb9b4
juanmagl1 <juanmabnobi1@gmail.com>
2022-02-11 13:42:38 +0100

    segunda version

--- src/main/HolaMundo.java
+++ src/main/HolaMundo.java
@@ -3,7 +3,7 @@ package main;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		// TODO cpmentario
+		// TODO cpmentario de un conflicto
 		System.out.println("Hola mundo");
 	}
 
