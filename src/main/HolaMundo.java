67009db91128804844c9e323a11e6cae84c7fbed
juanmagl1 <74763054+juanmagl1@users.noreply.github.com>
2022-02-11 13:40:03 +0100

    Update HolaMundo.java

--- src/main/HolaMundo.java
+++ src/main/HolaMundo.java
@@ -3,7 +3,7 @@ package main;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO cpmentario
 		System.out.println("Hola mundo");
 	}
 
